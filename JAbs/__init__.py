92c32ddf19bc34ac5695b30f5f0a93718e023b33
KOLANICH <kolan_n@mail.ru>
2023-10-16 00:30:07 +0300

    We had to move to Codeberg

--- JAbs/__init__.py
+++ JAbs/__init__.py
@@ -1,6 +1,9 @@
 __all__ = ("SelectedJVMInitializer", "ClassPathT", "ClassesImportSpecT")
 import sys
 from importlib import import_module
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KAbs/JAbs.py , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
 
 from .utils.pathListTools import ClassesImportSpecT, ClassPathT
 
